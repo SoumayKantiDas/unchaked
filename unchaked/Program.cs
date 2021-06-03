ac6651902fc4acd212bbc01e3a87cacda69ba346
Soumay kanti Das <anikds2859@outlook.com>
2021-06-03 14:50:15 +0600

    Add project files.

---
+++ unchaked/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace unchaked
+{
+    class Testclass
+    {
+        const int x = 2147483647;
+        const int y = 2;
+        static void Main()
+        {
+            int z;
+            unchaked
+                {
+                z = x*y;
+            }
+            Console.WriteLine("unchaked output value: {0}", z);
+            
+        }
+    }
+}
